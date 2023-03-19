e341661b51c5f337c22927d127b4ad0b0efaebcf
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2023-03-19 18:03:58 +0530

    add java-style doc comments

--- include/trie.hpp
+++ include/trie.hpp
@@ -1,25 +1,86 @@
+/**
+ * @file trie.hpp
+ * @author Shriram R
+ * @brief header file for trie
+ * @date 2023-03-19
+ *
+ */
 #pragma once
 #include <vector>
 #include <memory>
 #include <string>
 
-namespace f7t {
-    // trie node
-    struct TrieNode {
+/**
+ * @brief namespace for foresight,
+ * contains a struct for trie node and a class for trie data structure
+ *
+ */
+namespace f7t
+{
+    /**
+     * @brief struct for trie node
+     *
+     */
+    struct TrieNode
+    {
         char c;
         bool isWord;
         std::vector<std::shared_ptr<TrieNode>> children;
-        TrieNode(char c): c(c), isWord(false) {}
+
+        /**
+         * @brief Construct a new Trie Node object
+         *
+         * @param c character
+         */
+        TrieNode(char c) : c(c), isWord(false) {}
     };
 
-    // trie
-    class Trie {
+    /**
+     * @brief class for trie data structure
+     *
+     */
+    class Trie
+    {
         std::shared_ptr<TrieNode> root;
-        void insert(std::shared_ptr<TrieNode> node, const std::string& word, size_t i);
-        void suggest(std::shared_ptr<TrieNode> node, const std::string& word, std::vector<std::string>& suggestions);
+
+        /**
+         * @brief insert word into trie node
+         *
+         * @param node trie node
+         * @param word word to be inserted
+         * @param i index of word
+         */
+        void insert(std::shared_ptr<TrieNode> node, const std::string &word, size_t i);
+
+        /**
+         * @brief suggest words that start with word
+         *
+         * @param node trie node
+         * @param word partial word on which suggestions are to be made
+         * @param suggestions vector of suggestions
+         */
+        void suggest(std::shared_ptr<TrieNode> node, const std::string &word, std::vector<std::string> &suggestions);
+
     public:
-        Trie(): root(std::make_shared<TrieNode>(' ')) {}
-        void insert(const std::string& word);
-        std::vector<std::string> suggest(const std::string& word);
+        /**
+         * @brief Construct a new Trie object
+         *
+         */
+        Trie() : root(std::make_shared<TrieNode>(' ')) {}
+
+        /**
+         * @brief insert word into trie
+         *
+         * @param word word to be inserted
+         */
+        void insert(const std::string &word);
+
+        /**
+         * @brief suggest words that start with word
+         *
+         * @param word partial word on which suggestions are to be made
+         * @return vector of words that are suggestions for given word
+         */
+        std::vector<std::string> suggest(const std::string &word);
     };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
-// program to suggest autocomplete words as user types
-// words are from usr/share/dict/words
-// words are stored in a trie
+/**
+ * @file main.cpp
+ * @author Shriram R
+ * @brief program to suggest autocomplete words as user types
+ * words are from usr/share/dict/words
+ * words are stored in a trie
+ * @date 2023-03-19
+ *
+ */
 
 #include <iostream>
 #include <fstream>
@@ -13,34 +19,51 @@
 #include <app.hpp>
 #include <spdlog/sinks/basic_file_sink.h>
 
-// read words from file
-std::vector<std::string> readWords(const std::string& filename) {
+/**
+ * @brief read words from file
+ *
+ * @param filename file name that contains the words
+ * @return vector of words from the file
+ */
+std::vector<std::string> readWords(const std::string &filename)
+{
     std::ifstream file(filename);
     std::vector<std::string> words;
     std::string word;
-    while (file >> word) {
+    while (file >> word)
+    {
         words.push_back(word);
     }
     return words;
 }
 
-std::shared_ptr<spdlog::logger> make_logger(spdlog::level::level_enum level) noexcept {
-    try {
-        //current time
+/**
+ * @brief instantiates a logger
+ *
+ * @param level log level
+ * @return shared_ptr to logger instance
+ * @exceptsafe
+ */
+std::shared_ptr<spdlog::logger> make_logger(spdlog::level::level_enum level) noexcept
+{
+    try
+    {
+        // current time
         auto now = std::chrono::system_clock::now();
         auto logger = spdlog::basic_logger_mt("foresight", "logs/" + std::to_string(now.time_since_epoch().count()) + ".log");
         logger->set_level(level);
         return logger;
     }
-    catch (const spdlog::spdlog_ex& ex) {
+    catch (const spdlog::spdlog_ex &ex)
+    {
         std::cout << "Log init failed: " << ex.what() << std::endl;
         return nullptr;
     }
 }
 
-
-// main
-int main() {
+/// @brief main function
+int main()
+{
 
     static auto logger = make_logger(spdlog::level::info);
 
@@ -48,7 +71,8 @@ int main() {
 
     std::unique_ptr<f7t::App> app = std::make_unique<f7t::App>(std::move(words), logger);
 
-    while (!app->shouldClose()) {
+    while (!app->shouldClose())
+    {
         app->poll();
     }
 
